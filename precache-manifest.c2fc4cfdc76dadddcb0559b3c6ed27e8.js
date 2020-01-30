7a3971cfa66a0508d4723b0f6b7fde1ecce2c428
Yash Soni <yasoni12@in.ibm.com>
2020-01-30 12:12:23 +0530

    Updates

---
+++ precache-manifest.c2fc4cfdc76dadddcb0559b3c6ed27e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df567118547e2b92e207d2c9fd687f89",
+    "url": "/web-push-sender/index.html"
+  },
+  {
+    "revision": "4a81fc58a29ca6ee3a84",
+    "url": "/web-push-sender/static/css/main.185436ef.chunk.css"
+  },
+  {
+    "revision": "36efe390d55de333349f",
+    "url": "/web-push-sender/static/js/2.0677c346.chunk.js"
+  },
+  {
+    "revision": "97d4d9a496db1116e61c1427a39acd43",
+    "url": "/web-push-sender/static/js/2.0677c346.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a81fc58a29ca6ee3a84",
+    "url": "/web-push-sender/static/js/main.d3e03264.chunk.js"
+  },
+  {
+    "revision": "cbfd59e5117cacdfa403",
+    "url": "/web-push-sender/static/js/runtime-main.4a098d67.js"
+  }
+]);
